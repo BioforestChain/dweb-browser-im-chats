400e8019aec30be8e7b798ed4bdb8d95bc662c14
JasonMetal <JasonMetal@bagen.info>
2024-02-06 17:32:32 +0800

    :fire: gen.cmd

--- pkg/proto/gen.cmd
+++ pkg/proto/gen.cmd
@@ -1,8 +1,3 @@
-#protoc --go_out=plugins=grpc:./common --go_opt=module=github.com/BioforestChain/dweb-browser-im-chats/pkg/proto/common common/common.proto
-#protoc --go_out=plugins=grpc:./admin --go_opt=module=github.com/BioforestChain/dweb-browser-im-chats/pkg/proto/admin admin/admin.proto
-#protoc --go_out=plugins=grpc:./chat --go_opt=module=github.com/BioforestChain/dweb-browser-im-chats/pkg/proto/chat chat/chat.proto
-
-
-protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=require_unimplemented_servers=false:. --go-grpc_opt=paths=source_relative  common/common.proto
-protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=require_unimplemented_servers=false:. --go-grpc_opt=paths=source_relative  admin/admin.proto
-protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=require_unimplemented_servers=false:. --go-grpc_opt=paths=source_relative  chat/chat.proto
+protoc --go_out=plugins=grpc:./common --go_opt=module=github.com/BioforestChain/dweb-browser-im-chats/pkg/proto/common common/common.proto
+protoc --go_out=plugins=grpc:./admin --go_opt=module=github.com/BioforestChain/dweb-browser-im-chats/pkg/proto/admin admin/admin.proto
+protoc --go_out=plugins=grpc:./chat --go_opt=module=github.com/BioforestChain/dweb-browser-im-chats/pkg/proto/chat chat/chat.proto
